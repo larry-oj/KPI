e3b25787cbec6c03453732baf870f66006eff7db
larry-oj <66905431+larry-oj@users.noreply.github.com>
2021-11-23 18:15:34 +0200

    окно

---
+++ Algorythms and Data Structures/2nd year ADS/Lab4/Genetic Algorithm - Clique Problem/Clique.cs
@@ -0,0 +1,62 @@
+using System.Numerics;
+using System;
+
+namespace Lab4_1
+{
+    public class Clique
+    {
+        private int[,] _matrix;
+
+        public int[,] Matrix => _matrix;
+
+        public Clique(int size)
+        {
+            this._matrix = CreateGraph(size);
+        }
+
+        private int[,] CreateGraph(int size)
+        {
+            var random = new Random();
+            var graph = new int[size, size];
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    graph[i, j] = 0;
+                }
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = i; j < size; j++)
+                {
+                    if (i == j) continue;
+                    var decider = random.Next(0, 2);
+                    graph[i, j] = decider;
+                    graph[j, i] = decider;
+                }
+            }
+
+            return graph;
+        }
+
+        public void CreateGraph()
+        {
+            var graph = new int[,] 
+            {
+                { 0, 1, 1, 1, 0, 1, 0, 0, 1 },
+                { 1, 0, 1, 0, 0, 0, 0, 0, 1 },
+                { 1, 1, 0, 1, 0, 1, 1, 1, 1 },
+                { 1, 0, 1, 0, 1, 1, 1, 1, 1 },
+                { 0, 0, 0, 1, 0, 1, 0, 0, 1 },
+                { 1, 0, 1, 1, 1, 0, 0, 0, 1 },
+                { 0, 0, 1, 1, 0, 0, 0, 1, 1 },
+                { 0, 0, 1, 1, 0, 0, 1, 0, 1 },
+                { 1, 1, 1, 1, 1, 1, 1, 1, 0 }
+            };
+
+            this._matrix = graph;
+        }
+    }
+}
---
+++ Algorythms and Data Structures/2nd year ADS/Lab4/Genetic Algorithm - Clique Problem/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Lab4_1.Genetic;
+
+namespace Lab4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var clique = new Clique(300);
+
+            // clique.CreateGraph();
+
+            var genetic = new GeneticAlgorithm(clique, 3, 0.1);
+
+            genetic.Progress(1000);
+
+            System.Console.WriteLine("Iteration - " + genetic.BestIteration);
+            System.Console.WriteLine("Fitness - " + genetic.Best.Fitness);
+            foreach (var gene in genetic.Best.Chromosome)
+            {
+                System.Console.Write(gene);
+            }
+        }
+    }
+}
