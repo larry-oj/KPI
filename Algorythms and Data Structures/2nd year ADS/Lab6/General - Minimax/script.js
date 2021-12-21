02d4000992c414c9a0c7ad56c6d9c1c77e7fdfaa
larry-oj <66905431+larry-oj@users.noreply.github.com>
2021-12-21 19:17:14 +0200

    first version

--- Algorythms and Data Structures/2nd year ADS/Lab6/General - Minimax/script.js
+++ Algorythms and Data Structures/2nd year ADS/Lab6/General - Minimax/script.js
@@ -1,16 +1,16 @@
 // interface elements
 let pScoreCont = document.getElementById('p-score');
 let bScoreCont = document.getElementById('b-score');
-let tGeneral = document.getElementById('t-general');
-let tFour = document.getElementById('t-four');
-let tFh = document.getElementById('t-th');
-let tStreet = document.getElementById('t-street');
-let tSix = document.getElementById('t-6');
-let tFive = document.getElementById('t-5');
-let tFours = document.getElementById('t-4');
-let tThree = document.getElementById('t-3');
-let tTwo = document.getElementById('t-2');
-let tOne = document.getElementById('t-1');
+// let tGeneral = document.getElementById('t-general');
+// let tFour = document.getElementById('t-four');
+// let tFh = document.getElementById('t-th');
+// let tStreet = document.getElementById('t-street');
+// let tSix = document.getElementById('t-6');
+// let tFive = document.getElementById('t-5');
+// let tFours = document.getElementById('t-4');
+// let tThree = document.getElementById('t-3');
+// let tTwo = document.getElementById('t-2');
+// let tOne = document.getElementById('t-1');
 // let diceOne = document.getElementById('dice-1');
 // let diceOneCb = document.getElementById('dice-1-cb');
 // let diceTwo = document.getElementById('dice-2');
@@ -30,6 +30,35 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+class Player {
+    constructor() {
+        this.cCombo = {
+            "general" : false,
+            "four" : false,
+            "fh" : false,
+            "street" : false,
+            "6" : false,
+            "5" : false,
+            "4" : false,
+            "3" : false,
+            "2" : false,
+            "1" : false
+        };
+
+        this.score = 0;
+    }
+
+    get scoreValue() { return this.score; }
+
+    setScore(val) { this.score = val; }
+
+    get completedCombos() { return this.cCombo; }
+
+    markCombo(cName) {
+        this.cCombo[cName] = true;
+    }
+}
+
 class Dice {
     constructor(num) {
         this.dice = document.getElementById('dice-' + num);
@@ -38,17 +67,11 @@ class Dice {
         this.number = num;
     }
 
-    get diceNumber() {
-        return this.number;
-    }
+    get diceNumber() { return this.number; }
 
-    get diceScore() {
-        return this.score;
-    }
+    get diceScore() { return this.score; }
 
-    setCB(value = true) {
-        this.cb = value;
-    }
+    setCB(value = true) { this.cb.checked = value; }
 
     rollDice() {
         if (this.cb.checked == false) return;
@@ -64,6 +87,9 @@ class State {
         }
         this.dices = d;
         this.rollNumber = 0;
+        this.combo = {};
+        this.round = 1;
+        this.over = false;
     }
 
     get scores() {
@@ -74,14 +100,66 @@ class State {
         return combo;
     }
 
-    get value(isCombo = true) {
+    get isOver() { return this.over; }
+
+    get combination() { return this.combo; }
+
+    checkStreet(counts) {
+        var count = {};
+        counts.forEach(function(i) { count[i] = (count[i]||0) + 1;});
+        if (count['1'] == 5) {
+            if (counts[0] == 0 || counts[5] == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    roll() {
+        if (this.over) return;
+        if (this.rollNumber >= 3) return;
+
+        this.rollNumber++;
+        if (this.rollNumber == 1) {
+            this.dices.forEach(dice => {
+                dice.setCB();
+            });
+        }
+
+        this.dices.forEach(dice => {
+            dice.rollDice();
+        });
+    }
+
+    keep() {
+        if (this.rollNumber < 1) return;
+        var comboName = this.combo.name;
+        if (comboName != "" && comboName != undefined) {
+            if (playerTurn) {
+                player.completedCombos[comboName] = true;
+                player.score += this.combo.value;
+            }
+            else {
+                bot.completedCombos[comboName] = true;
+                bot.score += this.combo.value;
+            }
+        }
+        playerTurn = !playerTurn;
+        this.rollNumber = 0;
+        this.round += 1;
+        if (this.round > 20) {
+            this.over = true;
+        }
+    }
+
+    calcValue() {
         let counts = [0, 0, 0, 0, 0, 0];
         for (var i = 0; i < 5; i++) {
             switch (this.dices[i].score) {
                 case 1:
                     counts[0] += 1;
                     break;
-                
+
                 case 2:
                     counts[1] += 1;
                     break;
@@ -108,65 +186,163 @@ class State {
         var value = 0;
 
         if (counts.includes(5)) {
-            if (this.rollNumber == 1) {
-                value = 100;
-            } else { value = 60; }
-            name = "general";
+            if (playerTurn) {
+                if (player.completedCombos["general"] == false) {
+                    if (this.rollNumber == 1) {
+                        value = 100;
+                    } else { value = 60; }
+                    name = "general";
+                }
+            }
+            else {
+                if (bot.completedCombos["general"] == false) {
+                    if (this.rollNumber == 1) {
+                        value = 100;
+                    } else { value = 60; }
+                    name = "general";
+                }
+            }
         }
         else if (counts.includes(4)) {
-            if (this.rollNumber == 1) {
-                value = 45;
-            } else { value = 40; }
-            name = "four";
+
+            if (playerTurn) {
+                if (player.completedCombos["four"] == false) {
+                    if (this.rollNumber == 1) {
+                        value = 45;
+                    } else { value = 40; }
+                    name = "four";
+                }
+            }
+            else {
+                if (bot.completedCombos["four"] == false) {
+                    if (this.rollNumber == 1) {
+                        value = 45;
+                    } else { value = 40; }
+                    name = "four";
+                }
+            }
         }
         else if (counts.includes(3) && counts.includes(2)) {
-            if (this.rollNumber == 1) {
-                value = 35;
-            } else { value = 30; }
-            name = "fh";
+            if (playerTurn) {
+                if (player.completedCombos["fh"] == false) {
+                    if (this.rollNumber == 1) {
+                        value = 35;
+                    } else { value = 30; }
+                    name = "fh";
+                }
+            }
+            else {
+                if (bot.completedCombos["fh"] == false) {
+                    if (this.rollNumber == 1) {
+                        value = 35;
+                    } else { value = 30; }
+                    name = "fh";
+                }
+            }
         }
-        else if 
-    }
-
-    checkStreet(counts) {
-        var count = {};
-        counts.forEach(function(i) { count[i] = (count[i]||0) + 1;});
-        console.log(counts);
-        console.log(count);
-    }
-
-    roll() {
-        if (this.rollNumber > 3) return;
-
-        this.rollNumber++;
-        if (this.rollNumber == 1) {
-            this.dices.forEach(dice => {
-                dice.setCB();
-            });
+        else if (this.checkStreet(counts)) {
+            if (playerTurn) {
+                if (player.completedCombos["street"] == false) {
+                    if (this.rollNumber == 1) {
+                        value = 25;
+                    } else { value = 20; }
+                    name = "street";
+                }
+            }
+            else {
+                if (bot.completedCombos["street"] == false) {
+                    if (this.rollNumber == 1) {
+                        value = 25;
+                    } else { value = 20; }
+                    name = "street";
+                }
+            }
+        }
+        else {
+            if (playerTurn) {
+                for (var i = 6; i >= 1; i--) {
+                    if (player.completedCombos[i + ""] == false && counts[i - 1] != 0) {
+                        value = i * counts[i - 1];
+                        name = i + "";
+                        break;
+                    }
+                }
+            }
+            else {
+                for (var i = 6; i >= 1; i--) {
+                    if (bot.completedCombos[i + ""] == false && counts[i - 1] != 0) {
+                        value = i * counts[i - 1];
+                        name = i + "";
+                        break;
+                    }
+                }
+            }
         }
 
-        this.dices.forEach(dice => {
-            dice.rollDice();
-        });
-    }
-
-    keep() {
-        if (playerTurn) {
-
+        this.combo = {
+            "value": value,
+            "name": name
         }
-    }
-
-
-
-    updateBoard() {
-        
     }
 }
 
 
-// variables
-let pScore = 0;
-let bScore = 0;
-let state = new State();
+// variables and data
 let playerTurn = true;
+let player = new Player();
+let bot = new Player();
+let state = new State();
+
+
+// update interface
+function updateStats() {
+    pScoreCont.innerText = player.scoreValue;
+    bScoreCont.innerText = bot.scoreValue;
+    if (!playerTurn) {
+        for (var [key, value] of Object.entries(player.completedCombos)) {
+            if (value == true) {
+                document.getElementById('t-' + key + '-p')
+                    .classList.add('completed');
+            }
+        }
+    }
+    else {
+        for (var [key, value] of Object.entries(bot.completedCombos)) {
+            if (value == true) {
+                document.getElementById('t-' + key + '-b')
+                    .classList.add('completed');
+            }
+        }
+    }
+    
+    state.dices.forEach(dice => {
+        dice.dice.innerText = dice.diceScore;
+    });
+
+    if (state.isOver) {
+        if (player.scoreValue > bot.scoreValue) {
+            pScoreCont.innerText += ' - Winner!';
+        }
+        else {
+            bScoreCont.innerText += " - Winner!";
+        }
+    }
+}
+updateStats();
+
+
+// bind buttons
+function rollAction() {
+    state.roll();
+    state.calcValue();
+    updateStats();
+}
+function keepAction() {
+    state.calcValue();
+    state.keep();
+    updateStats();
+}
+rollButton.onclick = rollAction;
+keepButton.onclick = keepAction;
+
 
